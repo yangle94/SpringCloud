a513bbf259a20576ee2c3e49f178159037aee1f8
杨乐 <yangle@xueleyun.com>
2017-06-16 17:34:10 +0800

    hehe

---
+++ SpringBootApiGetWay/src/main/java/cn/ylapl/SpringBootApiGetWay.java
@@ -0,0 +1,28 @@
+/**
+ * Company
+ * Copyright (C) 2004-2017 All Rights Reserved.
+ */
+package cn.ylapl;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.SpringCloudApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.client.loadbalancer.LoadBalanced;
+import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.client.RestTemplate;
+
+/**
+ * @author yangle
+ * @version $Id SeleniumMontitoring.java, v 0.1 2017-01-21 下午5:18 yangle Exp $$
+ */
+@EnableZuulProxy
+@SpringCloudApplication
+public class SpringBootApiGetWay {
+
+    public static void main(String arg[]) {
+
+        SpringApplication.run(SpringBootApiGetWay.class, arg);
+    }
+}
