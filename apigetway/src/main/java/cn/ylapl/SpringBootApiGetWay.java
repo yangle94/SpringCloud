7bc1f339ce553f4254fbb6e76ef76edc6c3eea60
杨乐 <yangle@xueleyun.com>
2017-11-23 21:51:52 +0800

    完成打包

--- apigetway/src/main/java/cn/ylapl/SpringBootApiGetWay.java
+++ apigetway/src/main/java/cn/ylapl/SpringBootApiGetWay.java
@@ -5,14 +5,9 @@
 package cn.ylapl;
 
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.security.oauth2.client.EnableOAuth2Sso;
 import org.springframework.cloud.client.SpringCloudApplication;
-import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
-import org.springframework.cloud.client.loadbalancer.LoadBalanced;
 import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
-import org.springframework.context.annotation.Bean;
-import org.springframework.web.client.RestTemplate;
 
 /**
  * @author yangle
