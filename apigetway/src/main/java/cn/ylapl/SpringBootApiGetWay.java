0f88d8a638e65b307ef49702fe5e1ce802ad7923
杨乐 <yangle@xueleyun.com>
2017-12-01 11:09:03 +0800

    调整参数

--- apigetway/src/main/java/cn/ylapl/SpringBootApiGetWay.java
+++ apigetway/src/main/java/cn/ylapl/SpringBootApiGetWay.java
@@ -13,7 +13,7 @@ import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
  * @author yangle
  * @version $Id SeleniumMontitoring.java, v 0.1 2017-01-21 下午5:18 yangle Exp $$
  */
-//@EnableOAuth2Sso
+@EnableOAuth2Sso
 @EnableZuulProxy
 @SpringCloudApplication
 public class SpringBootApiGetWay {
