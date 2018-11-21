d043d1871d9e1ea6abdaed8ce671c74ec6e373fa
杨乐 <yangle@ewell.cc>
2018-11-21 15:33:24 +0800

    first commit

---
+++ apigetway/src/main/java/com/ylapl/cloud/apigetway/ApigetwayApplication.java
@@ -0,0 +1,12 @@
+package com.ylapl.cloud.apigetway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApigetwayApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ApigetwayApplication.class, args);
+    }
+}
---
+++ apigetway/src/test/java/com/ylapl/cloud/apigetway/ApigetwayApplicationTests.java
@@ -0,0 +1,16 @@
+package com.ylapl.cloud.apigetway;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class ApigetwayApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
